24440905024037e6ac1db018a1e0af7a944a1cbd
lbulla <118393328+lbulla@users.noreply.github.com>
2025-05-27 18:52:37 +0200

    Web integration

--- plugin-canvas/src/platform/os_window_handle.rs
+++ plugin-canvas/src/platform/os_window_handle.rs
@@ -1,5 +1,6 @@
 use std::{ops::Deref, rc::Rc};
 
+#[cfg(not(target_arch = "wasm32"))]
 use raw_window_handle::{HasDisplayHandle, HasWindowHandle};
 
 use super::window::OsWindow;
@@ -10,9 +11,7 @@ pub(crate) struct OsWindowHandle {
 
 impl OsWindowHandle {
     pub(super) fn new(os_window: Rc<OsWindow>) -> Self {
-        Self {
-            os_window,
-        }
+        Self { os_window }
     }
 }
 
@@ -24,14 +23,20 @@ impl Deref for OsWindowHandle {
     }
 }
 
+#[cfg(not(target_arch = "wasm32"))]
 impl HasWindowHandle for OsWindowHandle {
-    fn window_handle(&self) -> Result<raw_window_handle::WindowHandle<'_>, raw_window_handle::HandleError> {
+    fn window_handle(
+        &self,
+    ) -> Result<raw_window_handle::WindowHandle<'_>, raw_window_handle::HandleError> {
         self.os_window.as_ref().window_handle()
     }
 }
 
+#[cfg(not(target_arch = "wasm32"))]
 impl HasDisplayHandle for OsWindowHandle {
-    fn display_handle(&self) -> Result<raw_window_handle::DisplayHandle<'_>, raw_window_handle::HandleError> {
+    fn display_handle(
+        &self,
+    ) -> Result<raw_window_handle::DisplayHandle<'_>, raw_window_handle::HandleError> {
         self.os_window.as_ref().display_handle()
     }
 }
